313649804b6b18f7127bbe08efb9996699d86abc
PJunhyuk <jgravity@naver.com>
2017-09-29 21:17:29 +0900

    Initiate HW1-Python

---
+++ HW1-Python/HW1-2.py
@@ -0,0 +1,15 @@
+def bubble_sort(list):
+    ######################list를 입력받아 bubblesort 해주는 함수##################
+
+
+
+    
+    ##############################################################################
+
+list = [3,1,10,6,8,9,5,5,4,2]
+print("List Before Sort")
+print(list)
+
+bubble_sort(list)
+print("List After Sort")
+print(list)
